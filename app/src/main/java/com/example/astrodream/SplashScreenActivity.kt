2cb1891d5e809a9726f0cf0d3f179604f1eb1898
Raul Andrade <raul.andrade21@gmail.com>
2020-11-17 23:39:17 -0300

    Subindo splash screen

---
+++ app/src/main/java/com/example/astrodream/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.astrodream
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            callInitialActivity()
+            finish()
+        }, 3000)
+    }
+
+    fun callInitialActivity() {
+        val intent = Intent(this, InitialActivity::class.java)
+        startActivity(intent)
+    }
+}

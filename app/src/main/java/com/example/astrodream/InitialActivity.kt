3007230a0ba1b7cd309bd10d4cda3fa978444bde
artchier <ar-chaves@hotmail.com>
2020-11-17 23:08:59 -0300

    Cores e background

--- app/src/main/java/com/example/astrodream/InitialActivity.kt
+++ app/src/main/java/com/example/astrodream/InitialActivity.kt
@@ -2,10 +2,13 @@ package com.example.astrodream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_initial.*
 
-class MainActivity : AppCompatActivity() {
+class InitialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_initial)
+
+        tbInitial.inflateMenu(R.menu.menu)
     }
 }

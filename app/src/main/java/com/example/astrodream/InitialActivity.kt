8a9d6b1e13527ce5de8732b0e73472281ea152a6
artchier <ar-chaves@hotmail.com>
2020-11-21 02:18:36 -0300

    Implementação da Activity Inicial e do Menu Lateral

--- app/src/main/java/com/example/astrodream/InitialActivity.kt
+++ app/src/main/java/com/example/astrodream/InitialActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.example.astrodream.R.id.menu_lateral
 import kotlinx.android.synthetic.main.activity_initial.*
 
 class InitialActivity : AppCompatActivity() {
@@ -12,14 +15,27 @@ class InitialActivity : AppCompatActivity() {
         setContentView(R.layout.activity_initial)
 
         tbInitial.title = ""
-        tbInitial.setOnMenuItemClickListener {
-            dlInitial.open
-        }
         setSupportActionBar(tbInitial)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            menu_lateral -> {
+                dlInitial.openDrawer(GravityCompat.END)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if(dlInitial.isDrawerOpen(GravityCompat.END))
+            dlInitial.closeDrawer(GravityCompat.END)
+        else
+            finish()
     }
 }

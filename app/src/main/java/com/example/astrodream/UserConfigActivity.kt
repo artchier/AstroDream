3edd7be58bee8c0ebfa132a2459c27e168f06925
Rafael Santana <rafaelns.br@gmail.com>
2020-11-21 19:36:28 -0300

    Revert "Revert "Subindo Activity da tela de configurações""
    This reverts commit 19ffe91b

---
+++ app/src/main/java/com/example/astrodream/UserConfigActivity.kt
@@ -0,0 +1,23 @@
+package com.example.astrodream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import kotlinx.android.synthetic.main.activity_initial.*
+import kotlinx.android.synthetic.main.activity_user_config.*
+
+class UserConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_config)
+
+        tbUserConfig.title = ""
+        setSupportActionBar(tbUserConfig)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}

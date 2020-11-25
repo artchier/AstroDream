4432005c2d0895b1952855d4e812d8857ffef57f
Rafael Santana <rafaelns.br@gmail.com>
2020-11-25 17:28:21 -0300

    Adaptando AvatarActivity para usar ActivityWithTopBar

--- app/src/main/java/com/example/astrodream/ActivityWithTopBar.kt
+++ app/src/main/java/com/example/astrodream/ActivityWithTopBar.kt
@@ -1,16 +1,17 @@
 package com.example.astrodream
 
+import android.content.Intent
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.app_tool_bar.*
 
 abstract class ActivityWithTopBar(
@@ -23,7 +24,8 @@ abstract class ActivityWithTopBar(
 
     fun setUpMenuBehavior() {
         // cada botão do menu lateral (lateral_menu.xml) deve ser obtido dessa forma
-        val lateralMenu = findViewById<ConstraintLayout>(R.id.clLateralMenu)
+        val lateralMenu = findViewById<NavigationView>(R.id.nvLateralMenu)
+        val btnAvatar = lateralMenu.findViewById<AppCompatButton>(R.id.btnAvatar)
         val btnSobre = lateralMenu.findViewById<AppCompatButton>(R.id.btnSobre)
 
         toolBar = if (toolBar == null) appToolBar else toolBar
@@ -46,6 +48,15 @@ abstract class ActivityWithTopBar(
                 )
                 .setView(R.layout.astrodialog)
                 .show()
+        }
+
+        btnAvatar.setOnClickListener {
+            if (this is AvatarActivity) {
+                drawerLayout.closeDrawer(GravityCompat.END)
+            }
+            else {
+                startActivity(Intent(this, AvatarActivity::class.java))
+            }
         }
     }
 
--- app/src/main/java/com/example/astrodream/AvatarActivity.kt
+++ app/src/main/java/com/example/astrodream/AvatarActivity.kt
@@ -1,43 +1,16 @@
 package com.example.astrodream
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toDrawable
-import androidx.core.view.GravityCompat
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_avatar.*
-import kotlinx.android.synthetic.main.activity_globe.*
-import kotlinx.android.synthetic.main.buy_avatar_dialog.*
-import kotlinx.android.synthetic.main.lateral_menu.*
 
-class AvatarActivity : AppCompatActivity() {
+class AvatarActivity : ActivityWithTopBar(R.string.avatar, R.id.dlAvatar) {
+
     lateinit var listAvatar: List<Int>
     lateinit var adapter: AvatarAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_avatar)
-
-        //seta a Toolbar
-        tbAvatar.title = ""
-        setSupportActionBar(tbAvatar)
-
-        //clique do botão "Sobre"
-        btnSobre.setOnClickListener {
-            MaterialAlertDialogBuilder(this)
-                .setBackgroundInsetStart(70)
-                .setBackgroundInsetEnd(70)
-                .setBackgroundInsetTop(10)
-                .setBackgroundInsetBottom(100)
-                .setBackground(
-                    ContextCompat.getColor(this, android.R.color.transparent).toDrawable()
-                )
-                .setView(R.layout.astrodialog)
-                .show()
-        }
 
         listAvatar = listOf(
             R.drawable.ic_avatar_alien,
@@ -55,27 +28,7 @@ class AvatarActivity : AppCompatActivity() {
         adapter = AvatarAdapter(this, listAvatar)
 
         rvAvatar.adapter = adapter
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_lateral -> {
-                dlAvatar.openDrawer(GravityCompat.END)
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onBackPressed() {
-        if (dlAvatar.isDrawerOpen(GravityCompat.END))
-            dlAvatar.closeDrawer(GravityCompat.END)
-        else
-            startActivity(Intent(this, InitialActivity::class.java))
-            finish()
+        setUpMenuBehavior()
     }
 }

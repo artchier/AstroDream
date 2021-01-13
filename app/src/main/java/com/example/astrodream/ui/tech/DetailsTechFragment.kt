13ec6e0db5200c90e80ce560f92c4c4f36f17021
Raul Andrade <raul.andrade21@gmail.com>
2021-01-13 16:18:48 -0300

    Rescolvendo problema de duplicidade de itens no banco de dados na inserção

--- app/src/main/java/com/example/astrodream/ui/tech/DetailsTechFragment.kt
+++ app/src/main/java/com/example/astrodream/ui/tech/DetailsTechFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -69,6 +70,7 @@ class DetailsTechFragment : Fragment() {
 
         btnFavorTech.setOnClickListener {
             viewModel.addTechDB(Tech(techPiece[1], techPiece[2], techPiece[3]))
+            Toast.makeText(contextTechActivity, viewModel.msg, Toast.LENGTH_SHORT).show()
         }
 
         viewModel.getAllTechnologiesDB()
--- app/src/main/java/com/example/astrodream/ui/tech/DetailsTechViewModel.kt
+++ app/src/main/java/com/example/astrodream/ui/tech/DetailsTechViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.astrodream.ui.tech
 
-import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.astrodream.entitiesDatabase.Tech
@@ -8,16 +8,29 @@ import com.example.astrodream.services.ServiceDatabase
 import kotlinx.coroutines.launch
 
 class DetailsTechViewModel(val serviceDatabase: ServiceDatabase) : ViewModel(){
+    val listTech = MutableLiveData<List<Tech>>()
+    lateinit var msg: String
+
     fun addTechDB(tech: Tech) {
         viewModelScope.launch {
+
+            getAllTechnologiesDB()
+
+            listTech.value?.forEach {
+                if (tech.codReferenceTech == it.codReferenceTech) {
+                    msg = "Este item já está nos favoritos!"
+                    return@launch
+                }
+            }
+
             serviceDatabase.addTechTask(tech)
+            msg = "Item adicionado aos favoritos com sucesso!"
         }
     }
 
     fun getAllTechnologiesDB() {
         viewModelScope.launch {
-            var listTechnologies = serviceDatabase.getAllTechnologiesTask()
-            Log.i("Tecnologias", listTechnologies[0].toString())
+            listTech.value = serviceDatabase.getAllTechnologiesTask()
         }
     }
 }

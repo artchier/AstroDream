e3fbdb670413d0295994c3bfee5ba52372874f7c
Rafael Santana <rafaelns.br@gmail.com>
2021-01-26 22:48:09 -0300

    Removendo logs desnecessários

--- app/src/main/java/com/example/astrodream/services/SetWallpaperJob.kt
+++ app/src/main/java/com/example/astrodream/services/SetWallpaperJob.kt
@@ -5,7 +5,6 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.graphics.Point
 import android.graphics.drawable.Drawable
-import android.util.Log
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
@@ -17,14 +16,8 @@ import java.util.*
 
 class SetWallpaperJob : JobService() {
 
-    companion object {
-        const val TAG = "SetWallpaperJob"
-    }
-
     @SuppressLint("SimpleDateFormat")
     override fun onStartJob(params: JobParameters?): Boolean {
-
-        Log.d(TAG, "Job iniciado")
 
         GlobalScope.launch {
             val dateFormat = SimpleDateFormat("yyyy-MM-dd")
@@ -37,32 +30,25 @@ class SetWallpaperJob : JobService() {
                     // Handle successful response
                     is NetworkResponse.Success -> {
                         if ((response.body.url).contains("youtube")) {
-                            Log.d(TAG, "Imagem do youtube")
                             calendar.add(Calendar.DATE, -1)
                         } else {
                             imageUrl = response.body.hdurl
-                            Log.d(TAG, "Imagem encontrada: $imageUrl")
                         }
                     }
                     // Handle server error (unavailable date falls into ServerError)
                     is NetworkResponse.ServerError -> {
-                        Log.d(TAG, "Server error")
                         if (failCount++ >= 5) {
                             break
                         }
                         calendar.add(Calendar.DATE, -1)
                     }
                     // Qualquer outro tipo de resposta consideramos erro
-                    else -> {
-                        Log.d(TAG, "Generic error")
-                        break
-                    }
+                    else -> break
                 }
             }
 
             // Falhamos em buscar uma imagem, remarcamos o job
             if (imageUrl == "") {
-                Log.d(TAG, "Remarcando job")
                 jobFinished(params, true)
                 return@launch
             }
@@ -74,14 +60,12 @@ class SetWallpaperJob : JobService() {
                         resource: Drawable,
                         transition: Transition<in Drawable?>?
                     ) {
-                        Log.d(TAG, "Imagem baixada, usando como wallpaper")
                         val extras = params!!.extras
                         val screenSize = Point(extras.getInt("width"), extras.getInt("height"))
 
                         setImageAsWallpaper(screenSize, baseContext, resource)
                         // Terminamos o Job de fato
                         jobFinished(params, false)
-                        Log.d(TAG, "Job acabado!")
                     }
 
                     override fun onLoadCleared(placeholder: Drawable?) {}
@@ -94,7 +78,6 @@ class SetWallpaperJob : JobService() {
 
     override fun onStopJob(params: JobParameters?): Boolean {
         // A internet caiu enquanto baixávamos a imagem. True quer dizer que queremos remarcar o Job
-        Log.d(TAG, "Job interrompido")
         return true
     }
 

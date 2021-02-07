a3954af88696ce77de949c88dcc5eaebac12a169
Rafael Santana <rafaelns.br@gmail.com>
2021-02-07 19:02:47 -0300

    Adicionando funcionalidade de compartilhar e remover dos favoritos asteroides pelos favoritos

--- app/src/main/java/com/example/astrodream/ui/favorites/FavAsteroidFragment.kt
+++ app/src/main/java/com/example/astrodream/ui/favorites/FavAsteroidFragment.kt
@@ -7,10 +7,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.astrodream.R
-import com.example.astrodream.domain.Asteroid
+import com.example.astrodream.database.AppDatabase
 import com.example.astrodream.entitiesDatabase.AsteroidRoom
+import com.example.astrodream.services.ServiceDBAsteroidsImpl
+import com.example.astrodream.services.databaseReference
+import com.example.astrodream.services.service
+import com.example.astrodream.services.shareText
+import com.example.astrodream.ui.asteroids.AsteroidViewModel
 import kotlinx.android.synthetic.main.fragment_asteroid_favs.view.*
 
 class FavAsteroidFragment : Fragment() {
@@ -28,18 +37,32 @@ class FavAsteroidFragment : Fragment() {
         val url = asteroid.urlOrbit
         val descricao = asteroid.description
 
-        view.nome_asteroid_favs.text = "Asteroide $name"
-        view.findViewById<View>(R.id.asteroid_favs_fragment).setOnClickListener {
+        view.descricao_asteroids_favs.text = descricao
+        view.nome_asteroid_favs.text = getString(R.string.asteroid_template).format(name)
 
+        view.findViewById<View>(R.id.asteroid_favs_fragment).setOnClickListener {
+            val db = AppDatabase.invoke(requireActivity())
+            val serviceDB = ServiceDBAsteroidsImpl(db.asteroidDAO())
+            val viewModel by viewModels<AsteroidViewModel> {
+                object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                        return AsteroidViewModel(service, serviceDB, databaseReference) as T
+                    }
+                }
+            }
+            viewModel.deleteAsteroidInDB(asteroid)
+            requireActivity().onBackPressed()
         }
 
         view.btn_ver_orbita_favs.setOnClickListener {
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
         }
 
-        view.descricao_asteroids_favs.text = descricao
+        view.findViewById<ImageButton>(R.id.btnShareAsteroid).setOnClickListener {
+            shareText("Veja informações do asteróide $name", url, requireContext())
+        }
 
         return view
     }
